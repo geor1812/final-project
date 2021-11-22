a1c7c5773d852aff0cdd80b8845f4f59aff23a7a
georgeUrsachi <george.ursachi@lenus.io>
2021-11-22 21:02:20 +0100

    Login page styling

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,45 +1,54 @@
 import React from 'react'
-import { Container, Box, Card, TextField, Button, Stack } from '@mui/material'
+import {
+  Container,
+  Box,
+  Card,
+  TextField,
+  Button,
+  Stack,
+  Typography,
+} from '@mui/material'
 
 const Login = () => {
   return (
-    <Container>
-      <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        minHeight="100vh"
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="100vh"
+    >
+      <Card
+        sx={{ padding: '20px', pt: '50px', width: '500px', height: '500px' }}
       >
-        <Card
-          sx={{ padding: '20px', pt: '50px', width: '500px', height: '500px' }}
-        >
-          <form>
-            <Stack alignItems="center" spacing={3}>
-              <TextField
-                required
-                sx={{ width: '75%' }}
-                id="username"
-                label="Username"
-                variant="outlined"
-                //error={true}
-                type="text"
-              />
-              <TextField
-                required
-                sx={{ width: '75%' }}
-                id="password"
-                label="Password"
-                variant="outlined"
-                type="password"
-              />
-              <Button sx={{ width: '50%' }} variant="contained">
-                Log in
-              </Button>
-            </Stack>
-          </form>
-        </Card>
-      </Box>
-    </Container>
+        <form>
+          <Stack alignItems="center" spacing={3}>
+            <Typography variant="h5">LOG IN</Typography>
+            <Typography variant="p">Welcome to name-goes-here</Typography>
+            <TextField
+              required
+              sx={{ width: '75%' }}
+              id="username"
+              label="Username"
+              variant="outlined"
+              //error={true}
+              type="text"
+              autoComplete="off"
+            />
+            <TextField
+              required
+              sx={{ width: '75%' }}
+              id="password"
+              label="Password"
+              variant="outlined"
+              type="password"
+            />
+            <Button sx={{ width: '50%' }} variant="contained">
+              Log in
+            </Button>
+          </Stack>
+        </form>
+      </Card>
+    </Box>
   )
 }
 
